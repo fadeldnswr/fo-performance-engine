bcfa5c2a57c62ae61fbae003741168259702e116
fadeldnswr <fadel.achmad@ui.ac.id>
2026-01-01 20:16:37 +0700

    Main file as the CLI orchestrator

---
+++ cmd/main.go
@@ -0,0 +1,10 @@
+/*Fiber Optics Performance Engine CLI Application*/
+
+package main
+
+import "fmt"
+
+func main(){
+	// Placeholder for args parsing and algorithm calls
+	fmt.Println("This is a fiber optics performance engine CLI application.")
+}
