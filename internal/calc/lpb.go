c6d598e375eec10cc754d27d047b4be132ba5f88
fadeldnswr <fadel.achmad@ui.ac.id>
2026-01-01 20:23:27 +0700

    Create LPB and RTB calculation

---
+++ internal/calc/lpb.go
@@ -0,0 +1,55 @@
+package calc
+
+import (
+	"errors"
+)
+
+// Define struct for LPB inputs
+type LPBInputs struct {
+	TxPowerDbm float64
+	RxSensitivityDbm float64
+	FiberLossDb float64
+	ConnLossDb float64
+	SpliceLossDb float64
+	SplitterLossDb float64
+	SystemMarginDb float64
+	LinkLengthKm float64
+}
+
+// Define struct for LPB outputs
+type LPBResults struct {
+	TotalLossDb float64
+	RxPowerDbm float64
+	MarginDb float64
+	Status string
+}
+
+// Define function to calculate link power budget
+func CalculateLPB(input LPBInputs) (LPBResults, error) {
+	// Check if inputs are valid
+	if input.FiberLossDb < 0 {
+		return LPBResults{}, errors.New("Fiber loss does not have valid value")
+	}
+
+	// LPB Calculation logic
+	fiberAttenuation := input.FiberLossDb * input.LinkLengthKm
+	totalLoss := fiberAttenuation + input.ConnLossDb + input.SpliceLossDb + input.SpliceLossDb
+
+	// Received power and margin calculation: Pr = Pt - Ps or Total Loss
+	rxPower := input.TxPowerDbm - totalLoss
+	margin := rxPower - input.RxSensitivityDbm - input.SystemMarginDb
+
+	// Determine status
+	status := "FAIL"
+	if margin >= 0 {
+		status = "PASS"
+	}
+	
+	// Return results
+	return LPBResults{
+		TotalLossDb: totalLoss,
+		RxPowerDbm: rxPower,
+		MarginDb: margin,
+		Status: status,
+	}, nil
+}
---
+++ internal/calc/rtb.go
@@ -0,0 +1,51 @@
+package calc
+
+import (
+	"errors"
+)
+
+// Define struct for RTB inputs
+type RTBInputs struct {
+	BitrateGbps float64
+	TxRiseTimeNs float64
+	RxRiseTimeNs float64
+	FiberLengthKm float64
+	DispersionPerKm float64
+}
+
+// Define struct for RTB outputs
+type RTBResults struct {
+	TotalRiseTimeNs float64
+	AllowedRiseTimeNs float64
+	Status string
+}
+
+// Define function to calculate t_chromatic
+func CalculateTchrom(){}
+
+// Define function to calculate Rise Time Budget
+func CalculateRTB(input RTBInputs) (RTBResults, error){
+	// Check if inputs are valid
+	if input.BitrateGbps <= 0 {
+		return RTBResults{}, errors.New("Bitrate value does not have valid input")
+	}
+	// RTB Calculation logic
+	dispersion := input.FiberLengthKm * input.DispersionPerKm
+	systemRt := input.TxRiseTimeNs + input.RxRiseTimeNs + dispersion
+
+	// Allowed rise time calculation: Trx = 0.7 / Bitrate
+	allowedRt := 0.7 / input.BitrateGbps * 1000 // Convert to ns
+
+	// Determine status
+	status := "FAIL"
+	if systemRt <= allowedRt {
+		status = "PASS"
+	}
+	
+	// Return results
+	return RTBResults{
+		TotalRiseTimeNs: systemRt,
+		AllowedRiseTimeNs: allowedRt,
+		Status: status,
+	}, errors.New("An error has occured in Rise Time Budget calculations")
+}
